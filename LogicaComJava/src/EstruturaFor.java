ff01e49ea3b5c8c3f94737985563bf9a2903bf1b
Lucas <lucassouza104840@gmail.com>
2024-08-31 16:10:47 -0300

    Estrutura for!

--- LogicaComJava/src/EstruturaFor.java
+++ LogicaComJava/src/EstruturaFor.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class EstruturaFor {
+public class EstruturaFor {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
 }
